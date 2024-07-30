a267eef3bf12bb9fe0fd43f9f4c05efe925685d1
cosydark <cosydark@hotmail.com>
2024-07-31 02:10:24 +0800

    Leave Color Bake Tool

--- Assets/Editor/TextureUtility/TextureRemappingTool.cs
+++ Assets/Editor/TextureUtility/TextureRemappingTool.cs
@@ -13,18 +13,17 @@ namespace TextureUtilities
         // Unreal Private
         private Texture2D texture2D;
         // Real Private
-        private string LastTextureName = "initial";
-        private Vector2 resolution = new Vector2(0, 0);
+        private Vector2 resolution = Vector2.zero;
+        private Vector4 averageValue = Vector4.one;
         private ComputeBuffer computeShader;
-        private Channels channel;
         private RenderTexture renderTexture;
         
         [MenuItem("TA/Texture Remapping Tool")]
         public static void ShowWindow()
         {
             var window = ScriptableObject.CreateInstance<TextureRemappingTool>() as TextureRemappingTool;
-            window.minSize = new Vector2(720, 880 + 30);
-            window.maxSize = new Vector2(720, 880 + 30);
+            window.minSize = new Vector2(720, 880);
+            window.maxSize = new Vector2(720, 880);
             window.titleContent = new GUIContent("Texture Remapping Tool");
             window.ShowUtility();
         }
@@ -36,7 +35,7 @@ namespace TextureUtilities
 
         private void OnDestroy()
         {
-            CoreUtils.Destroy(renderTexture);
+            renderTexture.Release();
         }
 
         private void Setup()
@@ -45,23 +44,7 @@ namespace TextureUtilities
             renderTexture.Release();
             renderTexture = RenderTexture.GetTemporary(GetGrayValueRenderTextureDescriptor(resolution));
             renderTexture.filterMode = FilterMode.Point;
-            switch (channel)
-            {
-                case Channels.R:
-                    TextureUtility.SplitR(texture2D, renderTexture);
-                    break;
-                case Channels.G:
-                    TextureUtility.SplitG(texture2D, renderTexture);
-                    break;
-                case Channels.B:
-                    TextureUtility.SplitB(texture2D, renderTexture);
-                    break;
-                case Channels.A:
-                    TextureUtility.SplitA(texture2D, renderTexture);
-                    break;
-                default:
-                    throw new ArgumentOutOfRangeException();
-            }
+            TextureUtility.Initialize(texture2D, renderTexture);
         }
     
         private void OnGUI()
@@ -73,30 +56,30 @@ namespace TextureUtilities
             // Row 1
             EditorGUILayout.BeginHorizontal();
             EditorGUILayout.Space(3);
-                if (GUILayout.Button("Measure Range", GUILayout.Height(40), GUILayout.Width(140)))
+                if (GUILayout.Button("Compute", GUILayout.Height(40), GUILayout.Width(140)))
                 {
                     if (texture2D is null) {return;}
                     Setup();
-                    float average = 0;
-                    TextureUtility.MeasureMinMaxPixel(renderTexture, GetChannelIndexFromChannel(channel), ref average);
-                    
+                    averageValue = TextureUtility.GetAverage(renderTexture);
+                    TextureUtility.GetGrayValue(renderTexture, averageValue);
                 }
             EditorGUILayout.Space(3);
                 if (GUILayout.Button("Save", GUILayout.Height(40), GUILayout.Width(140)))
                 {
+                    TextureUtility.SaveRenderTextureToFile(renderTexture,
+                        GetTexturePath(texture2D).Replace(texture2D.name, $"{texture2D.name}_New"));
+                    AssetDatabase.Refresh();// Necessary?
                 }
-            EditorGUILayout.Space(3);
-                channel = (Channels)EditorGUILayout.EnumPopup("", channel, GUILayout.Width(90));
             EditorGUILayout.Space(3);
                 EditorGUILayout.LabelField($"Texture Info: <{resolution.x}, {resolution.y}>", AddColor(Color.white));
             GUILayout.FlexibleSpace();
             EditorGUILayout.EndHorizontal();
             // Row 2
             EditorGUILayout.Space(3);
-                // EditorGUILayout.LabelField($"Value Range: <{valueRange.x}, {valueRange.y}>", AddColor(GetColorFromChannel(channel)));
+                EditorGUILayout.LabelField($"Average Value: <{averageValue.x}, {averageValue.y}, {averageValue.z}, {averageValue.w}>");
                 if (texture2D != null)
                 {
-                    GUI.DrawTexture(new Rect(10, 150 + 50, 700, 700), renderTexture);
+                    GUI.DrawTexture(new Rect(10, 150, 700, 700), renderTexture);
                 }
                 else
                 {
@@ -117,35 +100,17 @@ namespace TextureUtilities
                 msaaSamples = 1,
             };
         }
-        private enum Channels {R ,G, B, A}
-
-        private Color GetColorFromChannel(Channels channel)
-        {
-            return channel switch
-            {
-                Channels.R => Color.red,
-                Channels.G => Color.green,
-                Channels.B => Color.blue,
-                Channels.A => Color.white,
-                _ => Color.magenta
-            };
-        }
-        private int GetChannelIndexFromChannel(Channels channel)
-        {
-            return channel switch
-            {
-                Channels.R => 0,
-                Channels.G => 1,
-                Channels.B => 2,
-                Channels.A => 3,
-                _ => 0
-            };
-        }
         private static GUIStyle AddColor(Color color)
         {
             GUIStyle style = new GUIStyle(GUI.skin.label);
             style.normal.textColor = color;
             return style;
+        }
+        static string GetTexturePath(Texture2D texture)
+        {
+            string relativePath = AssetDatabase.GetAssetPath(texture);
+            string absolutePath = System.IO.Path.GetFullPath(relativePath);
+            return absolutePath;
         }
     }
 }
--- Assets/Editor/TextureUtility/TextureUtility.cs
+++ Assets/Editor/TextureUtility/TextureUtility.cs
@@ -11,17 +11,16 @@ namespace TextureUtilities
 {
     public class TextureUtility
     {
-        public static void SplitR(Texture2D texture2D, RenderTexture renderTexture) => RGBASplit(texture2D, renderTexture, 0);
-        public static void SplitG(Texture2D texture2D, RenderTexture renderTexture) => RGBASplit(texture2D, renderTexture, 1);
-        public static void SplitB(Texture2D texture2D, RenderTexture renderTexture) => RGBASplit(texture2D, renderTexture, 2);
-        public static void SplitA(Texture2D texture2D, RenderTexture renderTexture) => RGBASplit(texture2D, renderTexture, 3);
-        public static Vector2 MeasureMinMaxPixel(Texture source, int channelIndex, ref float average) => MeasureMinMaxPixelValue(source, channelIndex, ref average);
-        private static readonly string ComputeShaderPath = "Assets/Editor/TextureUtility/CS_TextureUtility.compute";
+        public static void Initialize(Texture2D texture2D, RenderTexture renderTexture) => DispatchToRenderTexture(texture2D, renderTexture, 0);
+        public static void GetGrayValue(RenderTexture source, Vector4 averageValue) => ModifyGrayValueRange(source, averageValue);
+        public static Vector4 GetAverage(RenderTexture source) => MeasureAveragePixelValue(source);
         
+        // Measure Average Pixel Value
+        private static readonly string ComputeShaderPath = "Assets/Editor/TextureUtility/CS_TextureUtility.compute";
         private static readonly int MeasureSourceTexture = Shader.PropertyToID("_Measure_SourceTexture");
         private static readonly int MeasurePixelColor = Shader.PropertyToID("_Measure_PixelColor");
         private static readonly int MeasureResolution = Shader.PropertyToID("_Measure_Resolution");
-        private static Vector2 MeasureMinMaxPixelValue(Texture source, int channelIndex, ref float average)
+        private static Vector4 MeasureAveragePixelValue(RenderTexture source)
         {
             CommandBuffer cmd = new CommandBuffer();
             cmd.name = "Measure Texture";
@@ -38,60 +37,58 @@ namespace TextureUtilities
             cmd.Clear();
             // Fetch Data
             colorBuffer.GetData(pixelColors);
-            List<float> value = new List<float>();
-            float total = 0;
+            List<Vector4> value = new List<Vector4>();
+            Vector4 total = Vector4.zero;
             for (int i = 0; i < pixelColors.Length; i++)
             {
-                float v = GetPixelValueByIndex(pixelColors[i].Color, channelIndex);
+                Vector4 v = pixelColors[i].Color;
                 value.Add(v);
                 total += v;
             }
             colorBuffer.Release();
-            average = total / pixelColors.Length;
-            Vector2 minMax = new Vector2(value.Min(), value.Max());
-            return minMax;
+            total = total / pixelColors.Length;
+            return total;
         }
         
-        private static readonly int SourceTexture = Shader.PropertyToID("_RGBASplit_SourceTexture");
-        private static readonly int TextureIO = Shader.PropertyToID("_RGBASplit_TextureIO");
-        private static readonly int ChannelIndex = Shader.PropertyToID("_RGBASplit_ChannelIndex");
-        private static void RGBASplit(Texture2D source, RenderTexture renderTexture, int channelIndex)
-        {
-            ComputeShader cs = AssetDatabase.LoadAssetAtPath<ComputeShader>(ComputeShaderPath);
-            cs.SetTexture(RGBASplitKernel, SourceTexture, source);
-            cs.SetTexture(RGBASplitKernel, TextureIO, renderTexture);
-            cs.SetInt(ChannelIndex, channelIndex);
-            cs.Dispatch(RGBASplitKernel, renderTexture.width / 16, renderTexture.height / 16, 1);
-        }
-        private static float GetPixelValueByIndex(Color color, int channelIndex)
-        {
-            return channelIndex switch
-            {
-                0 => color.r,
-                1 => color.g,
-                2 => color.b,
-                3 => color.a,
-                _ => 0f
-            };
-        }
-        private static float GetPixelValueByIndex(Vector4 color, int channelIndex)
-        {
-            return channelIndex switch
-            {
-                0 => color.x,
-                1 => color.y,
-                2 => color.z,
-                3 => color.w,
-                _ => 0f
-            };
-        }
         private struct PixelColor
         {
             public Vector4 Color;
         }
-        private static readonly int RGBASplitKernel = 0;
+        // Dispatch To RenderTexture
+        private static readonly int SourceTexture = Shader.PropertyToID("_Initialize_SourceTexture");
+        private static readonly int TextureIO = Shader.PropertyToID("_Initialize_TextureIO");
+        private static void DispatchToRenderTexture(Texture2D source, RenderTexture renderTexture, int channelIndex)
+        {
+            ComputeShader cs = AssetDatabase.LoadAssetAtPath<ComputeShader>(ComputeShaderPath);
+            cs.SetTexture(InitializeKernel, SourceTexture, source);
+            cs.SetTexture(InitializeKernel, TextureIO, renderTexture);
+            cs.Dispatch(InitializeKernel, renderTexture.width / 16, renderTexture.height / 16, 1);
+        }
+        // Get Gray Scale Result
+        private static readonly int ModifyGrayValueRange_TextureIO = Shader.PropertyToID("_ModifyGrayValueRange_TextureIO");
+        private static readonly int ModifyGrayValueRange_AverageValue = Shader.PropertyToID("_ModifyGrayValueRange_AverageValue");
+        private static void ModifyGrayValueRange(RenderTexture source, Vector4 averageValue)
+        {
+            ComputeShader cs = AssetDatabase.LoadAssetAtPath<ComputeShader>(ComputeShaderPath);
+            cs.SetTexture(ModifyGrayValueRangeKernel, ModifyGrayValueRange_TextureIO, source);
+            cs.SetVector(ModifyGrayValueRange_AverageValue, averageValue);
+            cs.Dispatch(ModifyGrayValueRangeKernel, source.width / 16, source.height / 16, 1);
+        }
+        private static readonly int InitializeKernel = 0;
         private static readonly int MeasureMinMaxPixelValueKernel = 1;
-        private static readonly int InitialKernel = 2;
+        private static readonly int ModifyGrayValueRangeKernel = 2;
+        // Save To Disk
+        public static void SaveRenderTextureToFile(RenderTexture renderTexture, string filePath)
+        {
+            var oldRt = RenderTexture.active;
+            RenderTexture.active = renderTexture;
+            Texture2D tex = new Texture2D(renderTexture.width, renderTexture.height, TextureFormat.RGBAFloat, false, true);
+            tex.ReadPixels(new Rect(0, 0, renderTexture.width, renderTexture.height), 0, 0);
+            tex.Apply();
+            System.IO.File.WriteAllBytes(filePath, tex.EncodeToTGA());
+            RenderTexture.active = oldRt;
+        }
+
     }
 }
 
